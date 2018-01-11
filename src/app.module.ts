9f07b276f37b56fddc9602e4bdc08ad8f5b0ad10
_asdx_ <xasdx@tuta.io>
2018-01-11 18:10:59 +0000

    Bootstrap a server with an ApplicationModule

---
+++ src/app.module.ts
@@ -0,0 +1,4 @@
+import { Module } from "@nestjs/common"
+
+@Module({ imports: [], controllers: [], components: [] })
+export class ApplicationModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
-console.log("hello")
+import { NestFactory } from "@nestjs/core"
+import { ApplicationModule } from "./app.module"
+
+async function bootstrap() {
+  let app = await NestFactory.create(ApplicationModule)
+  await app.listen(3210)
+}
+
+bootstrap()
