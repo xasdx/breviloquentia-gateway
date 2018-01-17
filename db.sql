9bd506a828e745edfca00060aee3e9ff0fd1e583
_asdx_ <xasdx@tuta.io>
2018-01-17 17:24:41 +0000

    Add an sql initializer script for testing purposes

---
+++ db.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS 'test';
+USE 'test';
+
+CREATE TABLE IF NOT EXISTS 'service' (
+  'id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  'name' varchar(50) 
+) DEFAULT CHARSET=utf8;
